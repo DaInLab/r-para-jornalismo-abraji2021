250d890c659449d2a95ff418fdda6e69fd6d7817
Your Namejpalbino <jp.albino@unesp.br>
2022-08-20 15:58:27 -0300

    Atualização realizada em 2022-08-20.

--- live-coding-parte-2.R
+++ live-coding-parte-2.R
@@ -18,7 +18,8 @@ mortes_covid <- read_rds("dados-output/mortes_covid.Rds")
 
 mortes_covid %>%
   count()  # faz uma contagem
-
+#        n
+#1: 605473
 
 ## Pergunta 2: ---
 # Morreram mais mulheres ou mais homens de Covid-19?
@@ -26,6 +27,10 @@ mortes_covid %>%
 mortes_covid %>%
   count(cs_sexo) %>% # faz uma contagem por grupos
   arrange(desc(n)) # ordenar por alguma variável
+#   cs_sexo      n
+#1:       M 339715
+#2:       F 265694
+#3:       I     64
 
 ## Pergunta 3: ---
 # Quais estados têm o maior e o menor número de mortes,
@@ -42,9 +47,23 @@ View(mortes_estados) # função View() é útil para visualizar
 
 head(mortes_estados) # head é a função pra mostrar as primeiras
 # linhas de uma base
+#   sg_uf_not      n
+#1:        SP 159807
+#2:        RJ  70502
+#3:        MG  57891
+#4:        RS  36629
+#5:        PR  36617
+#6:        CE  26405
 
 tail(mortes_estados) # tail é a função pra mostrar as últimas
 # linhas de uma base
+#   sg_uf_not    n
+#1:        RO 6135
+#2:        SE 6126
+#3:        TO 3822
+#4:        AP 1777
+#5:        RR 1709
+#6:        AC 1636
 
 # municipios
 
@@ -54,6 +73,23 @@ mortes_municipios <- mortes_covid %>%
 
 head(mortes_municipios) # head é a função pra mostrar as primeiras
 # linhas de uma base
+#   sg_uf_not     id_municip     n
+#1:        SP      SAO PAULO 46136
+#2:        RJ RIO DE JANEIRO 39801
+#3:        CE      FORTALEZA 14773
+#4:        PE         RECIFE 11377
+#5:        DF       BRASILIA 11252
+#6:        PR       CURITIBA 10837
 
 tail(mortes_municipios) # tail é a função pra mostrar as últimas
 # linhas de uma base
+#   sg_uf_not              id_municip n
+#1:        TO            MURICILANDIA 1
+#2:        TO                  NAZARE 1
+#3:        TO             PALMEIRANTE 1
+#4:        TO                 SAMPAIO 1
+#5:        TO SAO MIGUEL DO TOCANTINS 1
+#6:        TO            WANDERLANDIA 1
+
+# Obs: alterações realizadas em: 20022/08/15
+# Apenas para ilustrar
